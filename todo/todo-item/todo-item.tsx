2059a029bc1f4e8ac704e7d80f089049037686f7
Emil Rune Møller <emilrmoeller@gmail.com>
2018-02-17 17:16:35 +0100

    updated css to work in edge and firefox

--- todo/todo-item/todo-item.tsx
+++ todo/todo-item/todo-item.tsx
@@ -6,11 +6,10 @@ import { withPreact, h } from '@framejs/renderer-preact';
     style: require('./todo-item.css')
 })
 class TodoItem extends withPreact(HTMLElement) {
-    @Attribute() checked: boolean = false;
+    @Attribute() checked: boolean;
     @Property() index: number;
     @Event() onTodoItemChecked: EventEmitter;
     @Event() onTodoItemRemove: EventEmitter;
-    
 
     handleOnRemove = () => this.onTodoItemRemove.emit(this.index);
     handleOnChecked = () => this.onTodoItemChecked.emit(this.index);
@@ -18,8 +17,9 @@ class TodoItem extends withPreact(HTMLElement) {
     render(): any {
         return (
             <li class={this.checked ? 'completed' : ''}>
-                <input type="checkbox" checked={this.checked} onChange={this.handleOnChecked} />
-                <label><slot></slot></label>
+                <svg hidden={!this.checked} class="icon" xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="-10 -18 100 135"><circle cx="50" cy="50" r="50" fill="none" stroke="#bddad5" stroke-width="3"/><path fill="#5dc2af" d="M72 25L42 71 27 56l-4 4 20 20 34-52z"/></svg>
+                <input type="checkbox" checked={this.checked} onChange={this.handleOnChecked}  id={this.index.toString() + '-checkbox'}/>
+                <label for={this.index.toString() + '-checkbox'}><slot></slot></label>
                 <button onClick={this.handleOnRemove}>x</button>
             </li>
         );
